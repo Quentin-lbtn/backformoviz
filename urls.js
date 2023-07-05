346ae73a1e91218b673d961e075b8497dd1893bc
Your name <Your email>
2023-07-05 15:06:21 +0200

    backend

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'backformoviz.vercel.app';
+const BACKEND_URL = 'https://backformoviz.vercel.app';
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'frontformoviz.vercel.app';
+const FRONTEND_URL = 'https://frontformoviz.vercel.app';
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
